fff42cfd2c5054bea2386689ad72a804ea908cbe
NicolasAcoss <nicolas@AC540-P19CSPAPP.urdom.ad.recouv>
2023-04-25 15:35:11 +0200

    Hotix

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@ release = '0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
+master_doc = 'index'
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
